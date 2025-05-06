d1e4ff7a7396c7bcfb31887252fbb6f7cea7c500
sangmok12 <amsangmok@gmail.com>
2025-05-06 17:24:17 +0900

    first commit

---
+++ src/variable/Var8.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647;
+    }
+}
---
+++ src/variable/ex/Var1ExQuestion.java
@@ -0,0 +1,6 @@
+package variable.ex;
+
+public class Var1ExQuestion {
+    public static void main(String[] args) {
+    }
+}
